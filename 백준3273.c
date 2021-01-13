d03ff8db0d3f25859ffccfc2040399fa1692c173
lightpurple <58844618+lightpurple@users.noreply.github.com>
2021-01-13 22:03:40 +0900

    21.01.13

---
+++ 백준3273.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp(const void *a, const void *b){
+	return *(int *)a - *(int *)b;
+}
+
+int main(){
+	int i,n,x,arr[100001] = {0,};
+	
+	scanf("%d", &n);
+	for(i=0; i<n; i++){
+		scanf("%d", &arr[i]);
+	}
+	scanf("%d", &x);
+	qsort(arr, n,sizeof(int),cmp);
+
+	int *fir = arr, *last = &arr[n-1];
+	int count=0;
+	
+	while(*fir<*last){
+		if((*fir) + (*last) == x){
+			count++;
+			fir++;
+		}else if((*fir) + (*last) > x){
+			last--;
+		}else{
+			fir++;
+		}
+	}
+	
+	printf("%d", count);
+	return 0;
+}
